bae1cf135b34177e31145f81d732edced5889d38
alexis <agschube+git@gmail.com>
2012-09-15 01:59:14 -0700

    here's the class

---
+++ DCard.py
@@ -0,0 +1,21 @@
+
+
+class Copper:
+    
+    def __init__(self):
+
+        self.name = 'Copper'
+        self.cost = 0
+        self.value = 1
+        self.victory_points = 0
+
+
+class Estate:
+    
+    def __init__(self):
+
+        self.name = 'Estate'
+        self.cost = 1
+        self.value = 0
+        self.victory_points = 1
+
